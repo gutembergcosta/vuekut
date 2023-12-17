6d6088ca7374ac7b32f9ec6186d89cc2eba06424
gutemberg <gutemberg.costa@loginfo.com.br>
2023-12-17 11:50:11 -0300

    review

--- src/main.js
+++ src/main.js
@@ -11,17 +11,14 @@ import { faPhone, faMoon, faEnvelope, faLock } from "@fortawesome/free-solid-svg
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { router } from './router/router'
 import PrimeVue from 'primevue/config';
-import Button from "primevue/button"
-
 
 const app = createApp(App);
 
 import App from '@/paginas/Pagina.vue'
-//import App from '@/exemplos/Diretivas.vue'
 
 library.add([faPhone,faMoon, faEnvelope, faLock]);
 
-app.component('Button', Button)
+app
 .use(router)
 .use(PrimeVue)
 .component("font-awesome-icon", FontAwesomeIcon)
